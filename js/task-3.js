48c9db50a4982dac90956932d70cca3553195a05
Artem Gardysh <artemgardysh@gmail.com>
2025-11-06 15:49:21 +0000

    task-3-update

--- js/task-3.js
+++ js/task-3.js
@@ -7,13 +7,13 @@ class StringBuilder {
     return this.#value;
   }
   padEnd(str) {
-    this.#value = this.#value + str;
+    return (this.#value = this.#value + str);
   }
   padStart(str) {
-    this.#value = str + this.#value;
+    return (this.#value = str + this.#value);
   }
   padBoth(str) {
-    this.#value = str + this.#value + str;
+    return (this.#value = str + this.#value + str);
   }
 }
 
